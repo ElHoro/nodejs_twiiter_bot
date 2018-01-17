94cf960685c450d3f950ecab4e4f8694e403cabd
Mike Huitrado <huitrado.mike@gmail.com>
2018-01-17 03:29:02 -0200

    Removed the language restriction

--- twitterBot.js
+++ twitterBot.js
@@ -14,20 +14,18 @@ let retweet = function() {
     let params = {
         q: 'leviosa',
         result_type: 'recent',
-        lang: 'en'
     }
 
     // search through all tweets using our params and execute a function:
-Twitter.get('search/tweets', params, function(err, data) {
+  Twitter.get('search/tweets', params, function(err, data) {
         // if there is no error
         if (!err) {
            // loop through the first 4 returned tweets
-          for (let i = 0; i < 2; i++) {
+          for (let i = 0; i < 10; i++) {
             // iterate through those first four defining a rtId that is equal to the value of each of those tweets' ids
           let rtId = data.statuses[i].id_str;
           let name = data.statuses[i].user.screen_name;
             // the post action
-          console.log(rtId);
           Twitter.post('statuses/update', {
             status: '@'+name+' You’re saying it wrong, It’s Wing-GAR-dium Levi-O-sa, make the “gar” nice and long.',
             // setting the id equal to the rtId variable
